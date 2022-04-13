efb16d22174c642031f775709c710810d7eeb9a5
winka9587_desktop <Administrator@WIN-10>
2022-04-13 17:12:41 +0800

    rescue my code :) finally, thank you,windows encoding

---
+++ preprocess/preprocess_utils.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+
+def load_depth(img_path):
+    """ Load depth image from img_path. """
+    depth_path = img_path + '_depth.png'
+    depth = cv2.imread(depth_path, -1)
+    if len(depth.shape) == 3:
+        # This is encoded depth image, let's convert
+        # NOTE: RGB is actually BGR in opencv
+        depth16 = depth[:, :, 1]*256 + depth[:, :, 2]
+        depth16 = np.where(depth16==32001, 0, depth16)
+        depth16 = depth16.astype(np.uint16)
+    elif len(depth.shape) == 2 and depth.dtype == 'uint16':
+        depth16 = depth
+    else:
+        assert False, '[ Error ]: Unsupported depth type.'
+    return depth16
